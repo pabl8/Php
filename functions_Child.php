7108f5902fdfa5e00b95fdf90933ee11a2a169d0
pabl8 <42950031+pabl8@users.noreply.github.com>
2020-07-30 00:52:28 +0200

    Remove Font Awesome from WordPress theme

--- functions_Child.php
+++ functions_Child.php
@@ -13,7 +13,7 @@ add_filter('wcmp_vendor_store_header_hide_store_phone', '__return_true');
 add_filter('wcmp_vendor_store_header_hide_store_email', '__return_true');
 
 /**
- * show sell by   --- comentada el 21/10 porque se agrego esta linea en la actualizacion del template de wyzi y ya no es necesario agregarlo por codigo
+ * show sell by
  
 
 add_filter( 'wcmp_sold_by_text_after_products_shop_page', 'filter_wcmp_sold_by_text' );
@@ -31,6 +31,7 @@ if (is_product ())
 }
 }
 
+
 /**
  * hide Questions & Answers
  */
@@ -41,19 +42,25 @@ function woo_remove_product_tabs( $tabs ) {
    unset( $tabs['additional_information'] );  	// Remove the additional information tab
    return $tabs;
 }
+
+
 /**
  * hide escaparate wcmp
- */
+  */
+
 add_filter('wcmp_vendor_dashboard_nav', 'callback_wcmp_vendor_dashboard_nav', 99);
 function callback_wcmp_vendor_dashboard_nav($vendor_nav){
     
    unset($vendor_nav['store-settings']['submenu']['storefront']);
- 
-   return $vendor_nav;
+   unset($vendor_nav['businesses']); // for Stats / Reports
+       return $vendor_nav;
 }
+
+
 /**
  * hide progress bar wcmp
  */
+
 add_filter( 'wcmp_vendor_dashboard_show_progress_bar', 'filter_wcmp_vendor_dashboard_show_progress_bar', 10, 1 );
 function filter_wcmp_vendor_dashboard_show_progress_bar( $vendor) {
 unset( $vendor); 
@@ -62,15 +69,14 @@ unset( $vendor);
 /**
  * Change number of products that are displayed per page (shop page)
  */
-add_filter( 'loop_shop_per_page', 'new_loop_shop_per_page', 100 );
+add_filter( 'loop_shop_per_page', 'new_loop_shop_per_page', 60 );
 
 function new_loop_shop_per_page( $cols ) {
   // $cols contains the current number of products per page based on the value stored on Options -> Reading
   // Return the number of products you wanna show per page.
-  $cols = 100;
+  $cols = 30;
   return $cols;
 }
-
 /**
  * hide fields in checkout woocomerce
  */
@@ -88,6 +94,7 @@ function custom_override_default_address_fields( $address_fields ) {
     return $address_fields;
 }
 
+
 /**
  * hide my shop
  */
@@ -96,3 +103,71 @@ function filter_wcmp_vendor_dashboard_header_nav( $header_nav ) {
 	unset($header_nav['shop-link']); //remove Vendor Shop Link
 	return $header_nav;
 }
+
+/**
+ * removing phone checkout woocomerce :)
+ */
+add_filter( 'woocommerce_billing_fields', 'remove_billing_phone_field', 20, 1 );
+function remove_billing_phone_field($fields) {
+    $fields ['billing_phone']['required'] = false; // To be sure "NOT required"
+
+    $fields['billing_email']['class'] = array('form-row-wide'); // Make the field wide
+
+    unset( $fields ['billing_phone'] ); // Remove billing phone field
+    return $fields;
+}
+
+
+//* Remove Font Awesome from WordPress theme
+add_action( 'wp_print_styles', 'tn_dequeue_font_awesome_style' );
+function tn_dequeue_font_awesome_style() {
+      wp_dequeue_style( 'fontawesome' );
+      wp_deregister_style( 'fontawesome' );
+}
+
+
+/**
+--------------------- no usados ----------------------
+ *diseable google maps v1 no funciono lo guardo por las dudas
+
+
+function disable_google_map_api($load_google_map_api) {
+
+  $load_google_map_api = false;
+
+  return $load_google_map_api;
+
+}
+
+$plugins = get_option('active_plugins');
+$required_plugin = 'auto-location-pro/auto-location.php';
+
+if ( in_array( $required_plugin , $plugins ) ) {
+  add_filter('avf_load_google_map_api', 'disable_google_map_api', 10, 1);
+}
+ 
+/**
+ *diseable google maps v2 no funciono lo guardo por las dudas
+ 
+	 add_filter( 'avf_load_google_map_api', '__return_false' );
+
+/**
+ * change title from category blog title
+ 
+// Simply remove anything that looks like an archive title prefix ("Archive:", "Foo:", "Bar:").
+add_filter('get_the_archive_title', function ($title) {
+    return preg_replace('/^\w+: /', '', $title);
+});
+
+
+
+ 
+Remove archives 
+function meks_remove_wp_archives(){
+  //If we are on category or tag or date or author archive
+  if( is_category() || is_tag() || is_date() || is_author() ) {
+    global $wp_query;
+    $wp_query->set_404(); //set to 404 not found page
+  }
+}
+*/
