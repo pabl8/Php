75a92c38f6136a5a2683a6d5df5da50f096d3975
pabl8 <42950031+pabl8@users.noreply.github.com>
2020-07-30 03:30:17 +0200

    Agregue los cambios de botones para woocommerce

--- functions_Child.php
+++ functions_Child.php
@@ -126,6 +126,18 @@ function tn_dequeue_font_awesome_style() {
 }
 
 
+// To change add to cart text on single product page
+add_filter( 'woocommerce_product_single_add_to_cart_text', 'woocommerce_custom_single_add_to_cart_text' ); 
+function woocommerce_custom_single_add_to_cart_text() {
+    return __( 'Contactar', 'woocommerce' ); 
+}
+
+// To change add to cart text on product archives(Collection) page
+add_filter( 'woocommerce_product_add_to_cart_text', 'woocommerce_custom_product_add_to_cart_text' );  
+function woocommerce_custom_product_add_to_cart_text() {
+    return __( 'Lo quiero', 'woocommerce' );
+}
+
 /**
 --------------------- no usados ----------------------
  *diseable google maps v1 no funciono lo guardo por las dudas
